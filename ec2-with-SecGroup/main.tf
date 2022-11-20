2e7c79f59f3ca690b9076967b40d237cce24a776
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-11-20 13:02:55 +0100

    Update main.tf

--- ec2-with-SecGroup/main.tf
+++ ec2-with-SecGroup/main.tf
@@ -19,20 +19,7 @@ resource "aws_instance" "linux" {
   tags = {
     "Name" = "Linux-ec2"
   }
-  user_data = <<EOF
-#!/bin/bash
-yum update -y
-yum install httpd -y
-FOLDER="https://raw.githubusercontent.com/cagatayakk/AWS_Projects/main/Project-101-kittens-carousel-static-website-ec2/static-web"
-cd /var/www/html
-wget $FOLDER/index.html
-wget $FOLDER/cat0.jpg
-wget $FOLDER/cat1.jpg
-wget $FOLDER/cat2.jpg
-wget $FOLDER/cat3.png
-systemctl start httpd
-systemctl enable httpd
-EOF
+  user_data = filebase64("user_data.sh")
 }
 
 resource "aws_security_group" "my-SecGrp" {
