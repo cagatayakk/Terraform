878c0dba7d3c7b3d00f1e8dcd2ede6633d21648c
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-11-21 23:53:33 +0100

    Update asg.tf

--- Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/asg.tf
+++ Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/asg.tf
@@ -1,12 +1,11 @@
 resource "aws_autoscaling_group" "my_asg" {
   name                      = "my_asg"
   max_size                  = 3
-  min_size                  = 1
+  min_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 2
+  desired_capacity          = 1
   target_group_arns         = [aws_lb_target_group.my_tg.arn]
-  launch_configuration      = aws_launch_configuration.my_LC.name
   vpc_zone_identifier       = [aws_subnet.my_subnet_1.id, aws_subnet.my_subnet_2.id]
   enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity",
   "GroupInServiceInstances", "GroupTotalInstances"]
@@ -19,5 +18,9 @@ resource "aws_autoscaling_group" "my_asg" {
     key                 = "Name"
     value               = "my_asg"
     propagate_at_launch = true
+  }
+  launch_template {
+    id = aws_launch_template.my_LT.id
+    version = aws_launch_template.my_LT.latest_version
   }
 }
