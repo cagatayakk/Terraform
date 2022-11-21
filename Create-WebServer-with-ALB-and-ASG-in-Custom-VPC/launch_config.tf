b83bb4124ac5269ae6f3f530087fe7c48f1160c4
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-11-21 22:45:17 +0100

    Update launch_config.tf

--- Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/launch_config.tf
+++ Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/launch_config.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "my_LC" {
   name                        = "my_LC"
   image_id                    = "ami-0b0dcb5067f052a63"
   instance_type               = "t2.micro"
-  key_name                    = "My_Key"
+  key_name                    = "First_Key"
   security_groups             = [aws_security_group.sg_ec2.id]
   associate_public_ip_address = true
   user_data                   = file("userdata.sh")
