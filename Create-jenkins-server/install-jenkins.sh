51c605a5acdfac341b4c539bd081583caa008923
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2024-01-08 18:35:44 +0000

    Update install-jenkins.sh

--- Create-jenkins-server/install-jenkins.sh
+++ Create-jenkins-server/install-jenkins.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 yum update -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+hostnamectl set-hostname jenkins-server
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade
 amazon-linux-extras install java-openjdk11 -y
 amazon-linux-extras install epel -y
