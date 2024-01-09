fa3098e79a86cf727174fbae48c9d01354bd9165
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2024-01-09 10:04:20 +0000

    Added dynamic_ingress

--- Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/sg_ec2.tf
+++ Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/sg_ec2.tf
@@ -1,35 +1,26 @@
+variable "sg-ports" {
+  default = [80, 22, 443]
+}
+
 resource "aws_security_group" "sg_ec2" {
   name        = "sg_ec2"
+  dynamic "ingress" {
+    for_each = var.sg-ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+ egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   description = "Allow SSH,HTTP,HTTPS inbound traffic"
   vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "sg_ec2"
