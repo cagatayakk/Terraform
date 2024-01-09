f4e4407f9c5fee683acd7de96d463c0a4cd5dfca
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2024-01-09 10:21:48 +0000

    Update launch_template.tf

--- Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/launch_template.tf
+++ Create-WebServer-with-ALB-and-ASG-in-Custom-VPC/launch_template.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "my_LT" {
   name                        = "my_LT"
-  image_id                    = "ami-0b0dcb5067f052a63"
+  image_id                    = "ami-00b8917ae86a424c9"
   instance_type               = "t2.micro"
-  key_name                    = "First_Key"
+  key_name                    = "cagatayakk"
   vpc_security_group_ids      = [aws_security_group.sg_ec2.id]
   user_data                   = filebase64("userdata.sh")
 
